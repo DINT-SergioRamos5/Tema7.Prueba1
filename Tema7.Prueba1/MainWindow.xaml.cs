e6896eae7f28d3c0a502bdedc444ea499da83716
Sergio Ramos Santonja <sergioramos55@outlook.es>
2020-01-08 13:22:59 +0100

    Agregar archivos de proyecto.

---
+++ Tema7.Prueba1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema7.Prueba1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private BaseDatosInformeEntities contexto;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            contexto = new BaseDatosInformeEntities();
+
+            contexto.CLIENTES.Load();            
+
+            var consulta = from n in contexto.CLIENTES
+                           where n.genero == "Female"
+                           orderby n.nombre
+                           select n;
+
+            ClientesListBox.DataContext = new ObservableCollection<CLIENTE>(consulta.ToList());
+            
+
+        }
+    }
+}
